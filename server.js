e56137b87e096e1777a76ef59067093d7dd0a8ca
Arunram07 <81552496+Arunram07@users.noreply.github.com>
2021-09-06 10:45:13 +0530

    fix : front end url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://awesome-perlman-8281d8.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
