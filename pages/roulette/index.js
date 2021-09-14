7003d57af3d0311866da80e36e864a19b755fd7b
Arunram07 <arunram@zevocorp.in>
2021-09-14 17:17:38 +0530

    choke : code split

--- pages/roulette/index.js
+++ pages/roulette/index.js
@@ -82,12 +82,12 @@ const Roulette = ({ status }) => {
   });
 
   useEffect(() => {
-    // socket = io("http://18.116.115.108:5000", {
-    //   transports: ["websocket"],
-    // });
-    socket = io("http://localhost:5000", {
+    socket = io("http://18.116.115.108:5000", {
       transports: ["websocket"],
     });
+    // socket = io("http://localhost:5000", {
+    //   transports: ["websocket"],
+    // });
     if (typeof window.ethereum !== "undefined") {
       setInstallMetamask(false);
       let chainID = window.ethereum.chainId;
